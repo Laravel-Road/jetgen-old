e39d7d22cfc42014f317e4d57a583455612d59d6
Pedro Augusto <pams.pedro@gmail.com>
2021-02-10 22:06:16 -0300

    feat: add types classifications

--- src/SyntaxBuilders/AbstractSintaxBuilder.php
+++ src/SyntaxBuilders/AbstractSintaxBuilder.php
@@ -18,8 +18,65 @@ class AbstractSintaxBuilder
      */
     protected array $bluePrintTypes;
 
+    /**
+     * @var array
+     */
+    protected array $stringTypes;
+
+    /**
+     * @var array
+     */
+    protected array $integerTypes;
+
+    /**
+     * @var array
+     */
+    protected array $floatTypes;
+
+    /**
+     * @var array
+     */
+    protected array $dateTypes;
+
+    /**
+     * @var array
+     */
+    protected array $foreignTypes;
+
     public function __construct()
     {
         $this->bluePrintTypes = config('jetgen.blueprint_types');
+        $this->stringTypes = config('jetgen.string_types');
+        $this->integerTypes = config('jetgen.integer_types');
+        $this->floatTypes = config('jetgen.float_types');
+        $this->dateTypes = config('jetgen.date_types');
+        $this->foreignTypes = config('jetgen.foreign_types');
+    }
+
+    /**
+     * @param array $field
+     * @return bool
+     */
+    protected function hasForeignConstraint(array $field): bool
+    {
+        return in_array($field['type'], $this->foreignTypes);
+    }
+
+    /**
+     * Remove empty fields.
+     *
+     * @param array $fields
+     *
+     * @return array
+     */
+    protected function removeEmpty(array $fields): array
+    {
+        foreach ($fields as $key => $field) {
+            if ($field == '') {
+                unset($fields[$key]);
+            }
+        }
+
+        return $fields;
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -20,13 +20,18 @@ abstract class TestCase extends Orchestra
     }
 
     /**
-     * @param Application $app
+     * @param $app
      */
     protected function getEnvironmentSetUp($app): void
     {
         $configs = $this->configs();
 
         $app['config']->set('jetgen.blueprint_types', $configs['blueprint_types']);
+        $app['config']->set('jetgen.string_types', $configs['string_types']);
+        $app['config']->set('jetgen.integer_types', $configs['integer_types']);
+        $app['config']->set('jetgen.float_types', $configs['float_types']);
+        $app['config']->set('jetgen.date_types', $configs['date_types']);
+        $app['config']->set('jetgen.foreign_types', $configs['foreign_types']);
     }
 
     /**
